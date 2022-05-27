e1f6b3cadad4d5bb0e45cff974dfa62e010192ef
Evelissia <evelissia@gmail.com>
2022-05-28 02:53:15 +0600

    Добавьте файлы проекта.

---
+++ GameOfLife/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace GameOfLife
+{
+    partial class ConwayMain
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbGrid = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numSSize = new System.Windows.Forms.NumericUpDown();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnAdvance = new System.Windows.Forms.Button();
+            this.btnGo = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbGrid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numSSize)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbGrid
+            // 
+            this.pbGrid.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pbGrid.Location = new System.Drawing.Point(12, 35);
+            this.pbGrid.Name = "pbGrid";
+            this.pbGrid.Size = new System.Drawing.Size(904, 469);
+            this.pbGrid.TabIndex = 0;
+            this.pbGrid.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 522);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Размер ячейки:";
+            // 
+            // numSSize
+            // 
+            this.numSSize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.numSSize.Location = new System.Drawing.Point(136, 520);
+            this.numSSize.Maximum = new decimal(new int[] {
+            25,
+            0,
+            0,
+            0});
+            this.numSSize.Minimum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numSSize.Name = "numSSize";
+            this.numSSize.Size = new System.Drawing.Size(50, 22);
+            this.numSSize.TabIndex = 2;
+            this.numSSize.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.numSSize.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // btnReset
+            // 
+            this.btnReset.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnReset.Location = new System.Drawing.Point(226, 522);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 39);
+            this.btnReset.TabIndex = 3;
+            this.btnReset.Text = "Сброс";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnAdvance
+            // 
+            this.btnAdvance.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAdvance.Location = new System.Drawing.Point(649, 522);
+            this.btnAdvance.Name = "btnAdvance";
+            this.btnAdvance.Size = new System.Drawing.Size(129, 39);
+            this.btnAdvance.TabIndex = 4;
+            this.btnAdvance.Text = "Обновить";
+            this.btnAdvance.UseVisualStyleBackColor = true;
+            this.btnAdvance.Click += new System.EventHandler(this.btnAdvance_Click);
+            // 
+            // btnGo
+            // 
+            this.btnGo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnGo.Location = new System.Drawing.Point(784, 522);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(132, 39);
+            this.btnGo.TabIndex = 5;
+            this.btnGo.Text = "Начать";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // ConwayMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(942, 573);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.btnAdvance);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.numSSize);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pbGrid);
+            this.MinimumSize = new System.Drawing.Size(960, 620);
+            this.Name = "ConwayMain";
+            this.Text = "Conway\'s Game of Life";
+            this.Load += new System.EventHandler(this.ConwayMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbGrid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numSSize)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbGrid;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numSSize;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnAdvance;
+        private System.Windows.Forms.Button btnGo;
+    }
+}
+
---
+++ GameOfLife/Form1.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameOfLife
+{
+    public partial class ConwayMain : Form
+    {
+        Boolean InProgress;
+        public ConwayMain()
+        {
+            InitializeComponent();
+        }
+        private void ConwayMain_Load(object sender, EventArgs e)
+        {
+            CreateGridSurface();
+        }
+
+        // этот метод создает сетку и ячейки
+        private void CreateGridSurface()
+        {
+            Point locPoint;
+            Cell newCell;
+            Random random = new Random();
+
+            // определение кол-ва строк и столбцов в сетке на основе размера ячейки, выбранного в форме
+            int rows = (int)(pbGrid.Height / numSSize.Value);
+            int cols = (int)(pbGrid.Width / numSSize.Value);
+
+            // создание сетки в которой содержатся отдельные ячейки
+            // ссылаюсь на PictureBox для определения размера
+            using (Bitmap bmp = new Bitmap(pbGrid.Width, pbGrid.Height))
+
+            // создание графического объекта, который будет обеспечивать поверхность рисования
+            using (Graphics g = Graphics.FromImage(bmp))
+
+            // кисть для рисования объекта на поверхности изображения
+            using (SolidBrush cellBrush = new SolidBrush(Color.Pink))
+            {
+                // создание пустой поверхности и присвоение копии растового изображения
+                g.Clear(Color.Black);
+                pbGrid.Image = (Bitmap)bmp.Clone();
+
+                Cell.gridCells.Clear();
+
+                for (int y = 0; y < rows; y++)
+                {
+                    for (int x = 0; x < cols; x++)
+                    {
+                        locPoint = new Point((int)(x * numSSize.Value), (int)(y * numSSize.Value));
+                        newCell = new Cell(locPoint, x, y);
+                        newCell.IsAlive = (random.Next(100) < 15) ? true : false;
+                        // объект ячейки добавляется в список
+                        Cell.gridCells.Add(newCell);
+                    }
+
+                    // нанесение всех вновь созданных ячеек на сетку
+                    foreach (Cell cell in Cell.gridCells)
+                    {
+                        // проверяется каждая ячейка в списке
+                        // Если она живая, то наносится на сетку с помощью нового прямоугольника
+                        if (cell.IsAlive)
+                        {
+                            g.FillRectangle(cellBrush, new Rectangle(cell.Location,
+                                new Size((int)numSSize.Value - 1, (int)numSSize.Value - 1)));
+                        }
+                    }
+
+                    // объект Bitmap клонируется в свойство изображения PictureBox
+                    pbGrid.Image = (Bitmap)bmp.Clone();
+                }
+            }
+        }
+
+        // метод для вычисления следующих позоций ячеек и обновления сетки
+        private void GetNextState()
+        {
+            // 1. Живая клетка с < 2 живыми соседками умирает от одиночества
+            // 2. Живая клетка с 2 или 3 соседками живет до следующего поколения
+            // 3. Живая клетка с > 3 живыми соседками умирает от перенасиления
+            // 4. Любая мертвая клетка с ровно 3-мя живыми соседями становится живой
+
+            // Вычисление следующего статуса каждой ячейки
+            foreach (Cell cell in Cell.gridCells)
+            {
+                int activeCount = cell.LiveAdjacent();
+
+                if (cell.IsAlive)
+                {
+                    if ((activeCount < 2) || (activeCount > 3))
+                        cell.NextStatus = false;
+                    else
+                        cell.NextStatus = true;
+                }
+                else
+                {
+                    if (activeCount == 3)
+                        cell.NextStatus = true;
+                }
+
+            }
+
+            foreach(Cell cell in Cell.gridCells)
+            {
+                cell.IsAlive = cell.NextStatus;
+            }    
+
+            using (Bitmap bmp = new Bitmap(pbGrid.Width, pbGrid.Height))
+            using(Graphics g = Graphics.FromImage(bmp))
+            using(SolidBrush cellBrush = new SolidBrush(Color.Pink))
+            {
+                g.Clear(Color.Black);
+                foreach (Cell cell in Cell.gridCells)
+                {
+                    if (cell.IsAlive)
+                    {
+                        g.FillRectangle(cellBrush, new Rectangle(cell.Location,
+                                new Size((int)numSSize.Value - 1, (int)numSSize.Value - 1)));
+                    }
+                }
+
+                pbGrid.Image.Dispose();
+                pbGrid.Image = (Bitmap)bmp.Clone();
+            }
+        }
+
+
+
+        // класс - чертеж для каждого объекта ячейки, который появляется в сетке
+        public class Cell
+        {
+            public static List<Cell> gridCells = new List<Cell>();
+            private Point cLocation;
+
+            // св-ва cXPos и cYPos определяют положение ячеек в сетке
+            private int cXPos;
+            private int cYPos;
+            private Boolean cIsAlive;
+            private Boolean cNext;
+
+            // конструктор для поиска местоположения в сетке в пикселях для новой ячейки
+            // (используя класс Point и координаты х, у по отношению к другим ячейкам)
+            public Cell(Point location, int X, int Y)
+            {
+                this.Location = location;
+                this.YPos = Y;
+                this.XPos = X;
+            }
+
+            public Point Location
+            {
+                get { return cLocation; }
+                set { cLocation = value; }
+            }
+
+            public int XPos
+            {
+                get { return cXPos; }
+                set { cXPos = value; }
+            }
+
+            public int YPos
+            {
+                get { return cYPos; }
+                set { cYPos = value; }
+            }
+
+            // это св-во указывает, является ли отдельная ячейка живой или не живой (активная или нет)
+            public Boolean IsAlive
+            {
+                get { return cIsAlive; }
+                set { cIsAlive = value; }
+            }
+
+            // хранение следующих состояний ячеек
+            public Boolean NextStatus
+            {
+                get { return cNext; }
+                set { cNext = value; }
+            }
+
+
+            // функция для возврата количества активных соседних ячеек
+            public int LiveAdjacent()
+            {
+                int liveAdjacent = 0;
+
+                // перебор коллекции ячеек
+                foreach (Cell cell in Cell.gridCells)
+                {
+                    // проверка является ли просматриваемая ячейка активной
+                    if (cell.Location != this.Location && cell.IsAlive)
+                    {
+                        // значения X и Y ячейки должны накодится на расстоянии не боолее одной ячейки от текущей
+                        if(Math.Abs(cell.XPos - this.XPos) < 2 && Math.Abs(cell.YPos - this.YPos) < 2)
+                        {
+                            // каждый раз, когда ф-ия находит активную ячейку радом с текущей, она увеличивает счетчик
+                            liveAdjacent++;
+                        }
+                    }
+                }
+                return liveAdjacent;
+            }
+        }
+
+        // событие сброса сетки
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            CreateGridSurface();
+        }
+
+        private void btnAdvance_Click(object sender, EventArgs e)
+        {
+            GetNextState();
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            InProgress = !InProgress;
+            btnGo.Text = InProgress ? "Остановить" : "Начать";
+
+            while (InProgress)
+            {
+                GetNextState();
+                Application.DoEvents();
+            }
+
+        }
+
+    }
+}
